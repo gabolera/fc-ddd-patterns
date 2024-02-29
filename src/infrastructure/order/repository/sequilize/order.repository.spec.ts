083713c801cc8393b8a94756354586eaa42f57e8
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2024-02-29 13:06:27 -0300

    chore: :sparkles: finishing the challenge `order-repository` with tests

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -81,4 +81,131 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should find an order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", "123", [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const foundOrder = await orderRepository.find(order.id);
+
+    expect(foundOrder).toStrictEqual(order);
+  })
+
+  it("should update an order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("345", "Customer 2");
+    const address = new Address("Street 2", 2, "Zipcode 2", "City 2");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("10", "Product 10", 20);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      5
+    );
+
+    const order = new Order("1", customer.id, [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const newOrderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      3
+    );
+
+    const updatedOrder = new Order("1", customer.id, [newOrderItem]);
+
+    await orderRepository.update(updatedOrder);
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: "1",
+      customer_id: "345",
+      total: updatedOrder.total(),
+      items: [
+        {
+          id: newOrderItem.id,
+          name: newOrderItem.name,
+          price: newOrderItem.price,
+          quantity: newOrderItem.quantity,
+          order_id: "1",
+          product_id: "10",
+        },
+      ],
+    });
+  })
+
+  it("should a list of orders", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("345", "Customer 2");
+    const address = new Address("Street 2", 2, "Zipcode 2", "City 2");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("10", "Product 10", 20);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      5
+    );
+    const order = new Order("1", customer.id, [orderItem]);
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    
+    const productTwo = new Product("101", "Product 101", 80);
+    await productRepository.create(productTwo);
+    const orderItemTwo = new OrderItem(
+      "2",
+      productTwo.name,
+      productTwo.price,
+      productTwo.id,
+      5
+    );
+    const orderTwo = new Order("2", customer.id, [orderItemTwo]);
+    await orderRepository.create(orderTwo);
+    
+    const orders = await orderRepository.findAll();
+
+    expect([orders, orderTwo]).toEqual([orders, orderTwo]);
+  })
 });
